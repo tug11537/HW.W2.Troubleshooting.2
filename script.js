8741057e468cc26020655f5adceab98406ac2ae6
tug11537 <tug11537@temple.edu>
2025-01-26 12:40:32 -0500

    Update script.js
    Added commas, :, [, deleted unnecessary commas.

--- script.js
+++ script.js
@@ -2,27 +2,27 @@
 const myWinterCoat = {
     model: "650 Down Jacket",
     version: 2.0,
-    color: "black,
+    color: "black" ,
     brand: {
         name: "REI",
-        website; "https://rei.com",
+        website: "https://rei.com",
         phone: "+18004264840",
         headquarters: {
             city: "Sumner",
             state: "WA",
             dateFounded: 1938
         }
-    },,
-    hadHood false,
+    },
+    hadHood: false,
     bestUse: ["Casual", "Hiking"],
     fabric: "Recycled nylon taffeta",
     liningFabric: "Nylon taffeta",
-    insulated: true
+    insulated: true,
     insulationType: "down",
     insulation:	"650-fill-power down", 
     warmth: "Warmer",
     weight: {
-        amt 10.8,
+        amt: 10.8,
         unit: "ounces"
     }
 
